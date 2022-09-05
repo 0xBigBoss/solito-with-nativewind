acc773e37963919ef068f881883d7cca4ec618a9
BigBoss <bigboss@sendnodes.io>
2022-09-05 18:06:35 -0500

    upgrade to expo 46

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
 }
 
 const { withExpo } = require('@expo/next-adapter')
+const withFonts = require('next-fonts')
+const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')([
   'solito',
